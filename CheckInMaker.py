374219b6ab3a3cc3f0006b7e5b10cd7271f21cdf
Chintan Panchamia <chintan10.panchamia@gmail.com>
2016-01-11 18:41:40 -0500

    Update CheckInMaker.py

--- CheckInMaker.py
+++ CheckInMaker.py
@@ -4,10 +4,7 @@ import random
 # from tweepy import OAuthHandler
 # from tweepy import Stream
 #
-# access_token = "2206438826-TTfR3KNPQdKdo7HvlftIKE9Mdwc1xJwRCsIw3S5"
-# access_token_secret = "BAC8yk38M9ItUqH8boGsxi0z1OF7W1mBa2k5K8Ci7a30c"
-# consumer_key = "45opyWmnEdNNKqPJOQ3jJczXK"
-# consumer_key_secret = "iFt2MDExEY2BuYKgWXObBk7CcRFOPijbv6iVaBTeFb8ZVx5R7d"
+# 
 #
 
 
@@ -35,22 +32,16 @@ import sys
 #config = ConfigParser.ConfigParser()
 #config.read(".twitter")
 
-#consumer_key = config.get("apikey", "45opyWmnEdNNKqPJOQ3jJczXK")
-#consumer_secret = config.get("apikey", "iFt2MDExEY2BuYKgWXObBk7CcRFOPijbv6iVaBTeFb8ZVx5R7d")
-#access_token = config.get("token", "2206438826-TTfR3KNPQdKdo7HvlftIKE9Mdwc1xJwRCsIw3S5")
-#ccess_token_secret = config.get("token", "BAC8yk38M9ItUqH8boGsxi0z1OF7W1mBa2k5K8Ci7a30c")
-#stream_rule = config.get("app", "rule")
-#account_screen_name = config.get("socialcomputingp2", "chinpanz10").lower()
-#account_user_id = config.get("socialcomputingp2", "2206438826")
+
 
 
 
 
 account_screen_name = "chinpanz10"
-access_token = "2206438826-WhmCoFFcnCSRCBs5NahWh4Zp3RCkPgFmjsvlN4U"
-access_token_secret = "UFZ67WwOreS8bKVtIPZYZ570djzPVp1k1ddFcRVduCjBl"
-consumer_key = "hvPueLbHPaW8hS7nqjsNgxJEy"
-consumer_secret = "NFJkdCPup9O3YpOd4t4dkE8qBr4a2I2KK6ARJTmJl4e7oIyc8G"
+access_token = 
+access_token_secret = 
+consumer_key = 
+consumer_secret = 
 
 auth = OAuthHandler(consumer_key,consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
